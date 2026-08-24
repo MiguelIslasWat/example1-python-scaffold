41a28f2f992f0b2f3e5afeffd4cb09415d9a8043
Miguel Islas <miguel.islas@iteso.mx>
2026-08-23 19:40:51 -0600

    returning v1 again

--- test_hello.py
+++ test_hello.py
@@ -5,12 +5,10 @@ def test_add():
     assert add(1, 1) == 2
 
 
-"""
-from hello import random_hash
+# from hello import random_hash
 
+# def test_random_hash():
+#    result = random_hash()
 
-def test_random_hash():
-    result = random_hash()
-
-    assert isinstance(result, str)
-    assert len(result) == 64"""
+#    assert isinstance(result, str)
+#    assert len(result) == 64
