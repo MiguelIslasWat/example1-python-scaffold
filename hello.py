8b6b27c78c287189be28906fc0fc58f5e8c72689
Miguel Islas <miguel.islas@iteso.mx>
2026-08-23 19:29:18 -0600

    Update application with random hash

--- hello.py
+++ hello.py
@@ -1,7 +1,20 @@
-def add(x, y):
+'''def add(x, y):
     """This is an add function"""
     return x + y
 
 
 if __name__ == "__main__":
-    print(add(1, 1))
+    print(add(1, 1))'''
+
+import hashlib
+import random
+
+
+def random_hash():
+    """Generate a random SHA-256 hash."""
+    random_value = str(random.random())
+    return hashlib.sha256(random_value.encode()).hexdigest()
+
+
+if __name__ == "__main__":
+    print(random_hash())
--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,13 @@
-from hello import add
-
+"""from hello import add
 
 def test_add():
-    assert add(1, 1) == 2
+    assert add(1, 1) == 2"""
+
+from hello import random_hash
+
+
+def test_random_hash():
+    result = random_hash()
+
+    assert isinstance(result, str)
+    assert len(result) == 64
